dac75390fbad0ef936477f11b33c66e343e19460
gvalman <germanj@ibest.com.br>
2016-07-08 11:18:52 -0300

    Modals Funcionando
    Os modals estam visiveis agora só falta colocar para interagir com o
    banco de dados

--- ProjetoWeb3Maven/src/main/webapp/resources/Js/index.js
+++ ProjetoWeb3Maven/src/main/webapp/resources/Js/index.js
@@ -11,6 +11,37 @@ function Carregar() {
     });
 
     ModalLogin();
+    ModalImagem();
+}
+
+function ModalImagem() {
+    // Get the modal
+    var modal = document.getElementById('ModalImagem');
+
+// Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close-imagem")[0];
+
+// When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+}
+
+function OpenModalImagem(id) {
+    // Get the modal
+    var modal = document.getElementById('ModalImagem');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+    var Img = document.getElementById(id);
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    
+    console.log(id);
+    
+    modal.style.display = "block";
+    modalImg.src = Img.src;
+    modalImg.alt = Img.alt;
+    captionText.innerHTML = Img.alt;
 }
 
 function ModalLogin() {
